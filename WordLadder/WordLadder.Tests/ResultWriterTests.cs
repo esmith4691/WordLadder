997fbceba942354fbd1b442475abdef690085948
Em <emcc4691@gmail.com>
2018-02-07 06:01:35 -0800

    Add result writer static helper class

---
+++ WordLadder/WordLadder.Tests/ResultWriterTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder.Tests
+{
+    [TestFixture]
+    public class ResultWriterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Directory.CreateDirectory(Path.Combine(GetAssemblyDirectory(), "output"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Directory.Delete(Path.Combine(GetAssemblyDirectory(), "output"), recursive: true);
+        }
+
+        [Test]
+        public void Verify_result_written_correctly()
+        {
+            var content = new[] { "test", "results", "look", "like", "this" };
+            var filename = Path.Combine(GetAssemblyDirectory(), "output", "testResult.txt");
+            var result = ResultWriter.WriteResult(filename, content );
+
+            Assert.IsTrue(result);
+            var fileContent = File.ReadAllLines(filename);
+            Assert.AreEqual(content, fileContent);
+        }
+
+        string GetAssemblyDirectory()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyFileName = assembly.Location;
+            return assemblyFileName.Remove(assemblyFileName.LastIndexOf('\\'));
+        }
+    }
+}
---
+++ WordLadder/WordLadder/ResultWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder
+{
+    internal static class ResultWriter
+    {
+        internal static bool WriteResult(string filename, IEnumerable<string> content)
+        {
+            try
+            {
+                File.WriteAllLines(filename, content);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
