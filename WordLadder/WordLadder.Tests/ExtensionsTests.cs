1d92897d1d77085e0f19256da5be89cb60aad6fb
Em <emcc4691@gmail.com>
2018-02-07 06:56:15 -0800

    Add extension method to compare if words are one letter different

--- WordLadder/WordLadder.Tests/ExtensionsTests.cs
+++ WordLadder/WordLadder.Tests/ExtensionsTests.cs
@@ -28,5 +28,28 @@ namespace WordLadder.Tests
         {
             Assert.IsFalse("w_rd".IsValidWord());
         }
+
+
+        [TestCase("word", "aword")]
+        [TestCase("word", "wrd")]
+        
+        [TestCase("word", "word")]
+        [TestCase("word", "card")]
+        [Test]
+        public void Check_words_do_not_have_one_letter_different(string word, string otherWord)
+        {
+            Assert.IsFalse(word.IsOneLetterDifferent(otherWord));
+        }
+
+        [TestCase("word", "lord")]
+        [TestCase("word", "ward")]
+        [TestCase("word", "woed")]
+        [TestCase("word", "work")]
+        [TestCase("word", "wArd")]
+        [Test]
+        public void Check_words_do_have_one_letter_different(string word, string otherWord)
+        {
+            Assert.IsTrue(word.IsOneLetterDifferent(otherWord));
+        }
     }
 }
--- WordLadder/WordLadder/Utilities/Extensions.cs
+++ WordLadder/WordLadder/Utilities/Extensions.cs
@@ -14,5 +14,21 @@ namespace WordLadder
         {
             return word.Length == Constants.WordSize && word.HasOnlyLetters();
         }
+
+        internal static bool IsOneLetterDifferent(this string word, string otherWord)
+        {
+            if (word.Length != otherWord.Length || word == otherWord)
+                return false;
+
+            var numberOfLettersDifferent = 0;
+
+            for(var i = 0; i < word.Length; i ++)
+            {
+                if (word[i] != otherWord[i])
+                    numberOfLettersDifferent++;
+            }
+
+            return numberOfLettersDifferent == 1;
+        }
     }
 }
