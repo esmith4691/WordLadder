f783d8d81687fff1f15996e65a0e5e393edc77b2
Em <emcc4691@gmail.com>
2018-02-07 05:35:29 -0800

    Add class to create a word dictionary from provided filename.

---
+++ WordLadder/WordLadder.Tests/WordDictionaryTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder.Tests
+{
+    [TestFixture]
+    public class WordDictionaryTests
+    {
+        WordDictionary sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new WordDictionary();
+        }
+
+        [Test]
+        public void Instantiates_collection_correctly()
+        {
+            Assert.AreEqual(new List<string>(), sut.Words);
+        }
+
+        [Test]
+        public void Returns_false_if_file_does_not_exist()
+        {
+            var result = sut.TryLoad("FileDoesNotExist.txt");
+            Assert.IsFalse(result);
+            Assert.AreEqual(new List<string>(), sut.Words);
+        }
+
+        [Test]
+        public void Returns_correct_words()
+        {
+            var expectedWords = new[] { "WASH", "WISH" };
+            var filePath = GetFilePath("testFile.txt");
+            var result = sut.TryLoad(filePath);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(expectedWords, sut.Words);
+        }
+
+        string GetFilePath(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyFileName = assembly.Location;
+            var assemblyDirectory = assemblyFileName.Remove(assemblyFileName.LastIndexOf('\\'));
+            return Path.Combine(assemblyDirectory, "Resources", fileName);
+        }
+    }
+}
---
+++ WordLadder/WordLadder/WordDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder
+{
+    internal class WordDictionary
+    {
+        internal List<string> Words = new List<string>();
+
+        internal bool TryLoad(string filename)
+        {
+            var content = new List<string>();
+
+            try
+            {
+                content = File.ReadAllLines(filename).ToList();
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+
+            this.Words = GetValidWords(content).Select(w => w.ToUpper()).ToList();
+            return true;
+        }
+
+        private static IEnumerable<string> GetValidWords(IEnumerable<string> content)
+        {
+            return content.Select(w => w.Trim()).Where(w => !string.IsNullOrEmpty(w) && w.IsValidWord());
+        }
+    }
+}
