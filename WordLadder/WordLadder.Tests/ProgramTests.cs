069559d57f808dfcb1179bdc68f139a5ae0d7fd2
Em <emcc4691@gmail.com>
2018-02-07 00:36:33 -0800

    Add basic input and output for program. Add guards for expected number of parameters.

--- WordLadder/WordLadder.Tests/ProgramTests.cs
+++ WordLadder/WordLadder.Tests/ProgramTests.cs
@@ -6,5 +6,18 @@ namespace WordLadder.Tests
     [TestFixture]
     public class ProgramTests
     {
+        static readonly string expectedError = "Incorrect number of parameters: Please check the input and try again:";
+
+        [TestCase("too few params")]
+        [TestCase("some   extra spaces")]
+        [TestCase(" extra at start")]
+        [TestCase("extra at end ")]
+        [TestCase("just one too many params")]
+        [Test]
+        public void Error_shown_for_incorrect_number_of_parameters(string input)
+        {
+            var output = Program.ProcessRequest(input);
+            Assert.AreEqual(expectedError, output);
+        }
     }
 }
--- WordLadder/WordLadder/Program.cs
+++ WordLadder/WordLadder/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("WordLadder.Tests")]
 namespace WordLadder
 {
-    class Program
+    internal static class Program
     {
+        static readonly string desiredFormat = "<dictionaryFile> <startWord> <endWord> <resultFile>";
+
         static void Main(string[] args)
         {
+            Console.Write($"Please enter you request in the format '{desiredFormat}':");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var result = ProcessRequest(input);
+                Console.WriteLine(result);
+            }
+        }
+
+        internal static string ProcessRequest(string input)
+        {
+            try
+            {
+                var args = input.Split(' ').Select(a => a.Trim()).Where(a => !string.IsNullOrEmpty(a));
+                if (args.Count() != 4)
+                    return "Incorrect number of parameters: Please check the input and try again:";
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                return $"An error has occurred. Please try again:";
+            }
         }
     }
 }
