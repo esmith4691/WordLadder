08e99db6fb5bde692288f5d07fe4197b2ac5d45f
Em <emcc4691@gmail.com>
2018-02-07 05:39:40 -0800

    Hook dictionary into request process and test error message

--- WordLadder/WordLadder.Tests/RequestProcessorTests.cs
+++ WordLadder/WordLadder.Tests/RequestProcessorTests.cs
@@ -7,11 +7,20 @@ namespace WordLadder.Tests
     public class RequestProcessorTests
     {
         [Test]
-        public void Error_shown_for_incorrect_number_of_parameters()
+        public void Error_shown_for_invalid_parameters()
         {
             var input = "invalid params";
             var output = RequestProcessor.ProcessRequest(input);
             var expectedError = "Invalid parameters: Please check the input and try again:";
+            Assert.AreEqual(expectedError, output);
+        }
+
+        [Test]
+        public void Error_shown_for_dictionary_load_failure()
+        {
+            var input = "fileNotExists.txt WORD WORD file.txt";
+            var output = RequestProcessor.ProcessRequest(input);
+            var expectedError = "Error loading dictionary: Please check file 'fileNotExists.txt'";
             Assert.AreEqual(expectedError, output);
         }
     }
--- WordLadder/WordLadder/RequestProcessor.cs
+++ WordLadder/WordLadder/RequestProcessor.cs
@@ -11,6 +11,11 @@ namespace WordLadder
             if (requestParams == null)
                 return "Invalid parameters: Please check the input and try again:";
 
+
+            var wordDictionary = new WordDictionary();
+            if (!wordDictionary.TryLoad(requestParams.DictionaryFile))
+                return $"Error loading dictionary: Please check file '{requestParams.DictionaryFile}'";
+
             throw new NotImplementedException();
         }
     }
