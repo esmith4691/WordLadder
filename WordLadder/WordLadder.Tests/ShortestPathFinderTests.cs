9cf29a27f4585d2bc5e29ba694a5fd088d4d8206
Em <emcc4691@gmail.com>
2018-02-07 19:57:10 -0800

    Add shortest path finder class

---
+++ WordLadder/WordLadder.Tests/ShortestPathFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder.Tests
+{
+    [TestFixture]
+    public class ShortestPathFinderTests
+    {
+        ShortestPathFinderTests sut;
+
+        [Test]
+        public void CanInstantiate()
+        {
+            Assert.DoesNotThrow(() => new ShortestPathFinder(new [] { "word", "ward", "card", "care" }));
+        }
+
+        [Test]
+        public void CanInstantiateWithEmptyCollection()
+        {
+            Assert.DoesNotThrow(() => new ShortestPathFinder(new string[] { }));
+        }
+    }
+}
---
+++ WordLadder/WordLadder/ShortestPathFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder
+{
+    internal sealed class ShortestPathFinder
+    {
+        internal IEnumerable<string> Result { get; private set; }
+        private IEnumerable<string> possibleWords;
+
+        private ShortestPathFinder() { }
+        internal ShortestPathFinder(IEnumerable<string> words)
+        {
+            this.possibleWords = words;
+        }
+    }
+}
