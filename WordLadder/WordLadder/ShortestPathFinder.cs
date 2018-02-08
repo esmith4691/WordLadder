f1974f3a001943639784d61d020972c9b1e86f39
Em <emcc4691@gmail.com>
2018-02-07 20:24:14 -0800

    Create method to find shortest path and roughly flesh out.

--- WordLadder/WordLadder/ShortestPathFinder.cs
+++ WordLadder/WordLadder/ShortestPathFinder.cs
@@ -8,13 +8,57 @@ namespace WordLadder
 {
     internal sealed class ShortestPathFinder
     {
-        internal IEnumerable<string> Result { get; private set; }
-        private IEnumerable<string> possibleWords;
+        private List<string> possibleWords;
+        private List<string> wordsToProcess;
 
         private ShortestPathFinder() { }
         internal ShortestPathFinder(IEnumerable<string> words)
         {
-            possibleWords = words.Select(w => w.ToUpper());
+            possibleWords = words.Select(w => w.ToUpper()).ToList();
+            wordsToProcess = new List<string>();
+        }
+        internal IEnumerable<string> FindShortestPath(string startWord, string endWord)
+        {
+            startWord = startWord.ToUpper();
+            endWord = endWord.ToUpper();
+
+            wordsToProcess.Add(startWord);
+            possibleWords.Remove(startWord);
+
+            while (wordsToProcess.Any())
+            {
+                var currentWord = wordsToProcess.First();
+
+                var forwardSteps = GetPossibleNextWords(currentWord);
+
+                if (forwardSteps.Contains(endWord))
+                    return GetRouteTo(currentWord).Union(new[] { endWord });
+
+                CreateChains(forwardSteps);
+
+                foreach(var word in forwardSteps)
+                    possibleWords.Remove(word);
+
+                wordsToProcess.AddRange(forwardSteps);
+                wordsToProcess.RemoveAt(0);
+            }
+
+            return new List<string>();
+        }
+
+        private void CreateChains(IEnumerable<string> nextSteps)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<string> GetRouteTo(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<string> GetPossibleNextWords(string word)
+        {
+            throw new NotImplementedException();
         }
     }
 }
