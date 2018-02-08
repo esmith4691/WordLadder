6ff33dc0cadc4e23c1dbdfe306c9ed61b94d39d9
Em <emcc4691@gmail.com>
2018-02-07 20:41:01 -0800

    Add word chain entity

---
+++ WordLadder/WordLadder.Tests/WordChainTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder.Tests
+{
+    [TestFixture]
+    public class WordChainTests
+    {
+        [Test]
+        public void Initialises_chain_correctly()
+        {
+            var sut = new WordChain("word", "anotherWord");
+
+            Assert.AreEqual("anotherWord", sut.EndWord);
+            Assert.AreEqual(new[] { "word", "anotherWord" }, sut.Path);
+        }
+
+        [Test]
+        public void Adds_another_word_correctly()
+        {
+            var sut = new WordChain("word", "anotherWord");
+            sut.AddWordToChain("finalWord");
+
+            Assert.AreEqual("finalWord", sut.EndWord);
+            Assert.AreEqual(new[] { "word", "anotherWord", "finalWord" }, sut.Path);
+        }
+    }
+}
---
+++ WordLadder/WordLadder/WordChain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLadder
+{
+    internal class WordChain
+    {
+        internal string EndWord { get; private set; }
+        internal List<string> Path { get; private set; }
+
+        internal WordChain(string startWord, string endWord)
+        {
+            EndWord = endWord;
+            Path = new List<string> { startWord, endWord };
+        }
+
+        internal void AddWordToChain(string word)
+        {
+            EndWord = word;
+            Path.Add(word);
+        }
+    }
+}
